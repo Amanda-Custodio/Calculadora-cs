f22a562588033f63c8233cdfce7aef16ab10b65f
Amanda Custódio <amandaccustodio@hotmail.com>
2022-07-18 15:34:40 -0300

    Exercício da Calculadora

---
+++ Exercicio-funcoes/Exercicio-funcoes/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace Exercicio_funcoes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int valor1, valor2, soma, subtracao, produto, divisao, i, n;
+
+            Console.WriteLine("Bem-vinda à Calculadora da Mallu! Aperte 1 para começar!");
+            n = int.Parse(Console.ReadLine());
+
+            if (n == 1)
+            {
+
+                do
+                {
+                    Console.WriteLine("Digite dois números para somar, subtrair, multiplicar e dividir");
+                    valor1 = int.Parse(Console.ReadLine());
+                    valor2 = int.Parse(Console.ReadLine());
+
+                    soma = valor1 + valor2;
+                    subtracao = valor1 - valor2;
+                    produto = valor1 * valor2;
+                    divisao = valor1 / valor2;
+
+                    Console.WriteLine("Para os números escolhidos, os resultados são: ");
+                    Console.WriteLine("Soma " + soma);
+                    Console.WriteLine("Subtração " + subtracao);
+                    Console.WriteLine("Multiplicação " + produto);
+                    Console.WriteLine("Divisão " + divisao);
+
+                    Console.WriteLine("Deseja continuar? Aperte 0 para prosseguir");
+                    i = int.Parse(Console.ReadLine());
+
+                }
+
+                while (i == 0);
+            }
+                
+                else Console.WriteLine("Fim do programa");
+            
+        }
+        public static int Soma(int x, int y)
+        {
+            return x + y;
+        }
+        public static int Subtracao(int x, int y)
+        {
+            return x - y;
+        }
+        public static int produto(int x, int y)
+        {
+            return x * y;
+        }
+        public static int divisao(int x, int y)
+        {
+            if (DiferenteDeZero(y)) throw new Exception("Não é permitido dividir por 0");
+            return x / y;
+
+            bool DiferenteDeZero(int y)
+            {
+                if (y == 0) return true;
+
+                return false;
+            }
+        }
+
+      
+    }
+}
